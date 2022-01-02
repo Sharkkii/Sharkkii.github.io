8b5f9a648177f01ecf8fd5a806ed344bb54cccf8
Sharkkii <jaws.sharkkii@gmail.com>
2022-01-02 18:18:45 +0900

    add entry-point component

---
+++ src/components/organisms/EntryPoint.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="entry-point">
+  <internal-link link="/">
+    <template v-slot:link>
+      <div class="entry-point-container">
+        <div class="entry-point-content">
+          <slot name="content">
+          </slot>
+        </div>
+      </div>
+    </template>
+  </internal-link>
+</div>
+</template>
+
+<style lang="scss">
+.entry-point {
+
+  .entry-point-container {
+
+    border-color: $main-color-50;
+    border-style: solid;
+    background-color: $white;
+
+    transition-property: border-color, color, padding, border-width;
+    transition-duration: 1s;
+    &:hover {
+      border-color: $main-color-20;
+      color: $main-color-20;
+    }
+
+  }
+
+  .entry-point-content {
+
+  }
+
+}
+</style>
+
+<script>
+import InternalLink from "../atoms/InternalLink.vue"
+export default {
+  name: "entry-point",
+  components: {
+    InternalLink
+  }
+}
+</script>
